87d5046eb966b61e98c166c0e456332f9421cafd
jerieson <jeriesonasis>
2026-02-03 00:42:11 +0800

    refactor: customer routes import

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-import customerRoutes from './routes/customerRoutes';
+import customerRoutes from './routes/Customerroutes';
 
 dotenv.config();
 
